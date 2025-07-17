cc61a2b6db22f91cb8dc9e39763de83b47c061da
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-07-17 12:21:41 +0530

    Number Guassing Game

---
+++ project/numguassing.js
@@ -0,0 +1,11 @@
+   const prompt = require('prompt-sync')();
+   
+   let  num=prompt("enter a number:");
+   let gamenum=45;
+
+   while(num != gamenum){
+      num=prompt("you guase wrong number. try again: ");
+   }
+  
+   console.log("you win");
+     
